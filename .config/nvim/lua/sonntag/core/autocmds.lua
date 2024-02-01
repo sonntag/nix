06203425b6532539004832f018cdfbe06252aa96
Justin Sonntag <justin.sonntag@gmail.com>
2024-02-01 14:50:52 -0800

    Toggle off relative line numbers when in insert mode

--- .config/nvim/lua/sonntag/core/autocmds.lua
+++ .config/nvim/lua/sonntag/core/autocmds.lua
@@ -3,3 +3,15 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = {"*"},
     command = "normal zR",
 })
+
+-- Automatically set absolute line numbering when entering insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+    pattern = {"*"},
+    command = "set norelativenumber",
+})
+
+-- Automatically set relative line numbering when leaving insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    pattern = {"*"},
+    command = "set relativenumber",
+})
