a987297dbe100a1f093f94a614b083ed308eaf02
Justin Sonntag <justin.sonntag@gmail.com>
2024-10-31 14:12:09 -0700

    Add zoxide fish plugin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    # Source the Nord theme dircolors
+    eval (gdircolors -c ~/.dircolors)
+
+    # Initialize zoxide and alias it to d
+    zoxide init --cmd cd fish | source
+
+    # Initialize starship
+    starship init fish | source
 end
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
@@ -12,12 +21,3 @@ if test -f /opt/homebrew/Caskroom/mambaforge/base/bin/conda
     eval /opt/homebrew/Caskroom/mambaforge/base/bin/conda "shell.fish" "hook" $argv | source
 end
 # <<< conda initialize <<<
-
-# Source the Nord theme dircolors
-eval (gdircolors -c ~/.dircolors)
-
-# Initialize zoxide and alias it to d
-zoxide init --cmd cd fish | source
-
-# Initialize starship
-starship init fish | source
