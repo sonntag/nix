7b9d82032283779e673d8b61b59b0537e391c10d
Justin Sonntag <justin.sonntag@gmail.com>
2025-01-02 15:53:27 -0800

    delete old color schemes

--- nvim/lua/sonntag/plugins/colorscheme.lua
+++ nvim/lua/sonntag/plugins/colorscheme.lua
@@ -1,37 +1,5 @@
--- return {
--- 	"catppuccin/nvim",
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	config = function()
--- 		require("catppuccin").setup({})
--- 		vim.cmd.colorscheme("catppuccin")
--- 	end,
--- }
 return {
 	'rose-pine/neovim',
 	name = 'rose-pine',
 	config = function() vim.cmd.colorscheme('rose-pine') end,
 }
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("tokyonight").setup({
--- 			style = "night",
--- 			styles = {
--- 				comments = { italic = true },
--- 			},
--- 		})
---
--- 		vim.cmd.colorscheme("tokyonight")
--- 	end,
--- }
--- return {
--- 	"shaunsingh/nord.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd.colorscheme("nord")
--- 	end,
--- }
