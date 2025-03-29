d0118005c359bcd1ebdafae8c96c5b5684af33e4
Justin Sonntag <justin.sonntag@gmail.com>
2025-03-29 12:07:59 -0700

    Add my user to knowUsers on wrath

--- hosts/wrath/users.nix
+++ hosts/wrath/users.nix
@@ -2,7 +2,7 @@
   userName = "justin";
   userHome = "/Users/${userName}";
 in {
-  nix.settings.trusted-users = [userName];
+  users.knownUsers = [userName];
 
   users.users.${userName} = {
     name = userName;
