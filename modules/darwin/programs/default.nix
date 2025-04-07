61a9a821b8f0560073f8054fca343b80205682d3
Justin Sonntag <justin.sonntag@gmail.com>
2025-04-07 00:03:43 -0700

    Update flake

--- modules/darwin/programs/default.nix
+++ modules/darwin/programs/default.nix
@@ -1,8 +1,10 @@
 {
   imports = [
     ./casks.nix
-    ./mas.nix
+    # TODO: this keeps reinstalling all mas apps
+    # ./mas.nix
     ./personal-casks.nix
   ];
   programs.fish.enable = true;
+  programs.fish.babelfishPackage = true;
 }
