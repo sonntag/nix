0f3ec1ea31599de8fd8213599c8166191cab50e3
Justin Sonntag <justin.sonntag@gmail.com>
2025-04-07 11:18:49 -0700

    Add note about mas bug

--- modules/darwin/programs/default.nix
+++ modules/darwin/programs/default.nix
@@ -1,7 +1,10 @@
 {
   imports = [
     ./casks.nix
-    # TODO: this keeps reinstalling all mas apps
+    # TODO: this keeps reinstalling all mas apps because of a bug in mas
+    # detecting the current installed apps:
+    # https://github.com/mas-cli/mas/issues/724
+    # https://github.com/mas-cli/mas/issues/728
     # ./mas.nix
     ./personal-casks.nix
   ];
