23e8e0bbc38e4e1bec137acc88bc2776a129fb8d
Justin Sonntag <justin.sonntag@gmail.com>
2025-04-14 16:28:16 -0700

    Starting to build out nix profile

--- flakes/nixvim/config/languages/nix/default.nix
+++ flakes/nixvim/config/languages/nix/default.nix
@@ -1,1 +1,5 @@
-{}
+{pkgs, ...}: {
+  plugins.treesitter.grammarPackages = [
+    pkgs.vimPlugins.nvim-treesitter.builtGrammars.nix
+  ];
+}
--- flakes/nixvim/config/profiles/coder/default.nix
+++ flakes/nixvim/config/profiles/coder/default.nix
@@ -4,6 +4,8 @@
     ./plugins/telescope.nix
     ./plugins/treesitter.nix
     # ./keymaps.nix
+    # Adding this for testing purposes
+    ../../languages/nix
   ];
   plugins.sleuth.enable = true;
 }
