36938895f737c468fad6940e6cb4bbcb1bd04005
Justin Sonntag <justin.sonntag@gmail.com>
2025-04-17 22:12:31 -0700

    inherit keymap directly

--- flakes/nixvim/nvim/keymaps.nix
+++ flakes/nixvim/nvim/keymaps.nix
@@ -1,12 +1,14 @@
-{util, ...}: {
+{util, ...}: let
+  inherit (util) keymap;
+in {
   keymaps = [
-    (util.keymap "n" "<esc>" "<cmd>nohlsearch<cr>" "")
+    (keymap "n" "<esc>" "<cmd>nohlsearch<cr>" "")
 
-    (util.keymap "n" "<left>" "<cmd>echo 'Use h to move!!'<cr>" "")
-    (util.keymap "n" "<right>" "<cmd>echo 'Use l to move!!'<cr>" "")
-    (util.keymap "n" "<up>" "<cmd>echo 'Use k to move!!'<cr>" "")
-    (util.keymap "n" "<down>" "<cmd>echo 'Use j to move!!'<cr>" "")
+    (keymap "n" "<left>" "<cmd>echo 'Use h to move!!'<cr>" "")
+    (keymap "n" "<right>" "<cmd>echo 'Use l to move!!'<cr>" "")
+    (keymap "n" "<up>" "<cmd>echo 'Use k to move!!'<cr>" "")
+    (keymap "n" "<down>" "<cmd>echo 'Use j to move!!'<cr>" "")
 
-    (util.keymap ["i" "x" "n" "s"] "<C-s>" "<cmd>w<cr><esc>" "Save File")
+    (keymap ["i" "x" "n" "s"] "<C-s>" "<cmd>w<cr><esc>" "Save File")
   ];
 }
--- flakes/nixvim/nvim/layers/buffers.nix
+++ flakes/nixvim/nvim/layers/buffers.nix
@@ -5,6 +5,7 @@
   ...
 }:
 with lib; let
+  inherit (util) keymap keymapLua whichkeyGroup;
   cfg = config.sonntag.nvim.layers.buffers;
 in {
   options.sonntag.nvim.layers.buffers.enable = mkEnableOption "buffers layer" // {default = true;};
@@ -13,18 +14,27 @@ in {
     plugins.bufferline.enable = true;
 
     plugins.which-key.settings.spec = [
-      (util.whichkeyGroup "<leader>b" "[B]uffer")
+      (whichkeyGroup "<leader>b" "[B]uffer")
     ];
 
     keymaps = [
-      (util.keymap "n" "<S-h>" "<cmd>bprevious<cr>" "Prev Buffer")
-      (util.keymap "n" "<S-l>" "<cmd>bpnext<cr>" "Next Buffer")
+      (keymap "n" "<S-h>" "<cmd>bprevious<cr>" "Prev Buffer")
+      (keymap "n" "<S-l>" "<cmd>bpnext<cr>" "Next Buffer")
 
-      (util.keymap "n" "<leader>bb" "<cmd>e #<cr>" "Switch to Other Buffer")
+      (keymap "n" "<leader>bb" "<cmd>e #<cr>" "Switch to Other Buffer")
 
-      (util.keymapLua "n" "<leader>bd" "require('snacks').bufdelete()" "[D]elete Buffer")
-      (util.keymapLua "n" "<leader>bo" "require('snacks').bufdelete.other()" "Delete [O]ther Buffers")
-      (util.keymap "n" "<leader>bD" "<cmd>:bd<cr>" "Delete Buffer and Window")
+      (keymapLua "n" "<leader>bd" "Snacks.bufdelete()" "[D]elete Buffer")
+      (keymapLua "n" "<leader>bo" "Snacks.bufdelete.other()" "Delete [O]ther Buffers")
+      (keymap "n" "<leader>bD" "<cmd>:bd<cr>" "Delete Buffer and Window")
+
+      (keymap "n" "<leader>bp" "<Cmd>BufferLineTogglePin<CR>" "Toggle Pin")
+      (keymap "n" "<leader>bP" "<Cmd>BufferLineGroupClose ungrouped<CR>" "Delete Non-Pinned Buffers")
+      (keymap "n" "<leader>br" "<Cmd>BufferLineCloseRight<CR>" "Delete Buffers to the Right")
+      (keymap "n" "<leader>bl" "<Cmd>BufferLineCloseLeft<CR>" "Delete Buffers to the Left")
+      (keymap "n" "<S-h>" "<cmd>BufferLineCyclePrev<cr>" "Prev Buffer")
+      (keymap "n" "<S-l>" "<cmd>BufferLineCycleNext<cr>" "Next Buffer")
+      (keymap "n" "[b" "<cmd>BufferLineMovePrev<cr>" "Move buffer prev")
+      (keymap "n" "]b" "<cmd>BufferLineMoveNext<cr>" "Move buffer next")
     ];
   };
 }
