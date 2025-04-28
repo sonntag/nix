f5371b4d78680c96c8640a6b518a6af1867b3d66
Justin Sonntag <justin.sonntag@gmail.com>
2025-04-27 17:12:12 -0700

    Remove unused pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,5 @@ final: prev: {
   defaultbrowser = final.callPackage ./defaultbrowser.nix {};
   desktoppr = final.callPackage ./desktoppr.nix {};
   kanata = final.callPackage ./kanata.nix {};
-  kanata-daemon-shim = final.callPackage ./kanata-daemon-shim {};
   karabiner-driverkit = final.callPackage ./karabiner-driverkit.nix {};
 }
