1beaba99ea4ecc29da794ba75c315486c20d445f
Justin Sonntag <justin.sonntag@gmail.com>
2025-06-24 15:19:44 -0700

    Add age key for greed

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## TODO
 
-
 ## Steps for setting up
 
 1. Install MacOS command line tools
@@ -20,3 +19,14 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 ```
 nix run nix-darwin -- switch --refresh --flake github:sonntag/nix#<hostname>
 ```
+
+## Steps for adding new machine secrets
+
+1. Create new age key on the machine
+
+```
+age-keygen -o ~/Library/Application Support/sops/age/keys.txt
+```
+
+2. Copy the public key and add it to .sops.yaml
+1. Profit
--- modules/home/secrets/default.nix
+++ modules/home/secrets/default.nix
@@ -1,30 +1,36 @@
 {
-  inputs,
-  pkgs,
   config,
+  inputs,
+  lib,
+  pkgs,
   ...
-}: let
+}:
+with lib; let
+  cfg = config.sonntag.secrets;
   configHome = config.xdg.configHome;
   home = config.home.homeDirectory;
   isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
 in {
+  options.sonntag.secrets.enable = mkEnableOption "secrets";
   imports = [inputs.sops-nix.homeManagerModules.sops];
-  sops = {
-    age.keyFile =
-      if isDarwin
-      then "${home}/Library/Application Support/sops/age/keys.txt"
-      else "${configHome}/sops/age/keys.txt";
-    secrets = {
-      ".ssh/id_justin" = {
-        sopsFile = ./ssh/justin.personal.yaml;
-        key = "private-key";
-        path = "${home}/.ssh/id_justin";
+  config = mkIf cfg.enable {
+    sops = {
+      age.keyFile =
+        if isDarwin
+        then "${home}/Library/Application Support/sops/age/keys.txt"
+        else "${configHome}/sops/age/keys.txt";
+      secrets = {
+        ".ssh/id_justin" = {
+          sopsFile = ./ssh/justin.personal.yaml;
+          key = "private-key";
+          path = "${home}/.ssh/id_justin";
+        };
       };
     };
-  };
 
-  home.file = {
-    "./.ssh/id_justin.pub".source = ./ssh/justin.pub;
-    "./.ssh/config".source = ./ssh/config;
+    home.file = {
+      "./.ssh/id_justin.pub".source = ./ssh/justin.pub;
+      "./.ssh/config".source = ./ssh/config;
+    };
   };
 }
