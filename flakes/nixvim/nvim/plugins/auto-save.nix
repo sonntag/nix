00cc14d27e374bb338c2e6f437218dbf31756394
Justin Sonntag <justin.sonntag@gmail.com>
2025-06-30 11:23:53 -0700

    Disable git check for autosave

--- flakes/nixvim/nvim/plugins/auto-save.nix
+++ flakes/nixvim/nvim/plugins/auto-save.nix
@@ -4,15 +4,16 @@
     settings.condition.__raw = ''
       function(buf)
         -- Skip oil buffers
-        if vim.bo[buf].filetype == 'oil' then return false end
+        -- if vim.bo[buf].filetype == 'oil' then return false end
+        return vim.bo[buf].filetype ~= 'oil'
 
-        -- Get the full path of the buffer
-        local path = vim.api.nvim_buf_get_name(buf)
-        if path == "" then return false end
-
-        -- Check if the file is in a git repo
-        local filepath = vim.fn.expand('%:p:h')
-        return vim.fn.system('git -C ' .. filepath .. ' rev-parse --is-inside-work-tree 2>/dev/null') == "true\n"
+        -- -- Get the full path of the buffer
+        -- local path = vim.api.nvim_buf_get_name(buf)
+        -- if path == "" then return false end
+        --
+        -- -- Check if the file is in a git repo
+        -- local filepath = vim.fn.expand('%:p:h')
+        -- return vim.fn.system('git -C ' .. filepath .. ' rev-parse --is-inside-work-tree 2>/dev/null') == "true\n"
       end
     '';
   };
