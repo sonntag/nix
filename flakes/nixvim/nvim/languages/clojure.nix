a47b268197c4947577828f7e15aec35b3eb6c6e4
Justin Sonntag <justin.sonntag@gmail.com>
2025-07-30 13:37:23 -0700

    Fix clojure lsp config

--- flakes/nixvim/nvim/languages/clojure.nix
+++ flakes/nixvim/nvim/languages/clojure.nix
@@ -11,6 +11,11 @@ in {
   config = mkIf cfg.enable {
     sonntag.nvim.languages.lisp.enable = true;
 
+    lsp.servers.clojure_lsp = {
+      enable = true;
+      settings.root_markers = [".git"];
+    };
+
     plugins = {
       conjure.enable = true;
 
@@ -19,21 +24,8 @@ in {
       ];
 
       conform-nvim.settings = {
-        formatters = {
-          cljstyle.command = getExe pkgs.cljstyle;
-          cljstyle.cwd.__raw = ''
-            require("conform.util").root_file({ ".cljstyle" })
-          '';
-        };
-
-        formatters_by_ft = {
-          clojure = ["cljstyle"];
-        };
-      };
-
-      lsp.servers.clojure_lsp = {
-        enable = true;
-        # rootDir = "require('lspconfig.util').root_pattern('.git')";
+        formatters.cljstyle.command = getExe pkgs.cljstyle;
+        formatters_by_ft.clojure = ["cljstyle"];
       };
     };
   };
