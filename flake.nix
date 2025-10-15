a0ff1d37d2150ae00e3f0d3deae80a28d531eb14
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-15 12:33:07 -0700

    Temporarily disable cachix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   description = "Justin's nix config";
 
-  nixConfig = {
-    substitutors = [
-      "https://sonntag.cachix.org"
-    ];
-    trusted-public-keys = [
-      "sonntag.cachix.org-1:4rHXwjmC/EpRwVkCyH0xLQaoeZT9C0oq8TINGSap1Wk="
-    ];
-  };
+  # nixConfig = {
+  #   substitutors = [
+  #     "https://sonntag.cachix.org"
+  #   ];
+  #   trusted-public-keys = [
+  #     "sonntag.cachix.org-1:4rHXwjmC/EpRwVkCyH0xLQaoeZT9C0oq8TINGSap1Wk="
+  #   ];
+  # };
 
   inputs = {
     # ==== Core ====
