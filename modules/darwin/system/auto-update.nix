85e610d8bb8261d131f57f869dec0a01d4efab58
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-18 13:35:31 -0700

    Add logic for multiple nix providers

--- modules/darwin/system/auto-update.nix
+++ modules/darwin/system/auto-update.nix
@@ -1,14 +1,12 @@
 {
   config,
   lib,
-  inputs,
   ...
 }:
 with lib; let
   cfg = config.sonntag.system.auto-update;
 in {
   options.sonntag.system.auto-update.enable = mkEnableOption "auto-update";
-  config.nix.package = inputs.determinate.inputs.nix.packages.aarch64-darwin.default;
   config.services.comin = {
     inherit (cfg) enable;
     remotes = [
--- modules/darwin/system/nix.nix
+++ modules/darwin/system/nix.nix
@@ -1,34 +1,58 @@
 {
   config,
+  inputs,
   lib,
   pkgs,
   ...
 }:
 with lib; let
-  cfg = config.sonntag.darwin.nix;
+  cfg = config.sonntag.nix;
+  isDeterminate = cfg.provider == "determinate";
+  nix-packages = {
+    "determinate" = inputs.determinate.inputs.nix.packages.aarch64-darwin.default;
+    "nix" = pkgs.nix;
+    # TODO: This should be set in an overlay: https://lix.systems/add-to-config/#flake-based-configurations
+    "lix" = pkgs.lixPackageSets.stable.lix;
+  };
 in {
-  options.sonntag.darwin.nix = {
-    enable = mkEnableOption "nix management";
+  options.sonntag.nix = {
+    provider = mkOption {
+      type = lib.types.enum ["determinate" "nix" "lix"];
+      default = "determinate";
+      description = "The provider type for the nix system";
+    };
     trusted-users = mkOption {
-      description = "Nix trusted users";
-      default = ["justin"];
       type = types.listOf types.str;
+      default = ["root" "justin"];
+      description = "Nix trusted users";
     };
   };
-  config.nix = {
-    inherit (cfg) enable;
-    package = mkIf cfg.enable pkgs.lix;
-    settings = mkIf cfg.enable {
+  config = {
+    nix = {
+      enable = !isDeterminate;
+      package = nix-packages.${cfg.provider};
+      settings = mkIf (!isDeterminate) {
+        inherit (cfg) trusted-users;
+        substituters = [
+          "https://cache.lix.systems"
+        ];
+        trusted-public-keys = [
+          "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        ];
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      };
+    };
+
+    determinate-nix.customSettings = mkIf isDeterminate {
       inherit (cfg) trusted-users;
-      substituters = [
-        "https://cache.lix.systems"
-      ];
-      trusted-public-keys = [
-        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      ];
-      experimental-features = [
-        "nix-command"
-        "flakes"
+      # Enables parallel evaluation (remove this setting or set the value to 1 to disable)
+      eval-cores = 0;
+      extra-experimental-features = [
+        "build-time-fetch-tree" # Enables build-time flake inputs
+        "parallel-eval" # Enables parallel evaluation
       ];
     };
   };
