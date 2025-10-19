1c1a94dfaadf9a43755694e2e54f5d97b1e5094a
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-18 20:20:58 -0700

    auto-update from local branch

--- modules/darwin/system/auto-update.nix
+++ modules/darwin/system/auto-update.nix
@@ -15,6 +15,12 @@ in {
         url = "https://github.com/sonntag/nix.git";
         branches.main.name = "main";
       }
+      {
+        name = "local";
+        # TODO: Pull this from a common config
+        url = "/Users/justin/Development/sonntag/nix";
+        poller.period = 2;
+      }
     ];
   };
 }
