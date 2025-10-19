cac6b40dae77ecdd8e5db82f51fd0c10d041418b
Justin Sonntag <justin.sonntag@gmail.com>
2025-10-18 20:09:36 -0700

    refactor personal-casks.nix

--- modules/darwin/programs/personal-casks.nix
+++ modules/darwin/programs/personal-casks.nix
@@ -2,16 +2,15 @@
   config,
   lib,
   ...
-}: {
-  options = {
-    sonntag.darwin.personal-casks.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-    };
-  };
+}:
+with lib; let
+  cfg = config.sonntag.darwin.personal-casks;
+in {
+  options.sonntag.darwin.personal-casks.enable =
+    mkEnableOption "personal casks" // {default = true;};
 
   # These are the casks that should only be on my personal macs
-  config.homebrew.casks = lib.mkIf config.sonntag.darwin.personal-casks.enable [
+  config.homebrew.casks = lib.mkIf cfg.enable [
     "ableton-live-suite"
     "bambu-studio"
     "blackhole-2ch"
