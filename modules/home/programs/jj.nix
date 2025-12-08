cb54dbffb93a206ba4fd24d7096c29a3f3a2ee72
Justin Sonntag <justin.sonntag@gmail.com>
2025-12-08 10:53:47 -0800

    Use difftastic with jj

--- modules/home/programs/jj.nix
+++ modules/home/programs/jj.nix
@@ -2,9 +2,12 @@
   programs.jjui.enable = true;
   programs.jujutsu = with lib; {
     enable = true;
-    settings.user = {
-      name = "Justin Sonntag";
-      email = mkDefault "justin.sonntag@gmail.com";
+    settings = {
+      user = {
+        name = "Justin Sonntag";
+        email = mkDefault "justin.sonntag@gmail.com";
+      };
+      ui.diff-formatter = ["difft" "--color=always" "$left" "$right"];
     };
   };
 }
