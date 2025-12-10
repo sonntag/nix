ce99f6b670ac7f187aa08c4f60324eb58eabd065
Justin Sonntag <justin.sonntag@gmail.com>
2025-12-10 00:43:27 -0700

    Add jj helpers

--- modules/home/programs/jj.nix
+++ modules/home/programs/jj.nix
@@ -11,7 +11,33 @@
         name = "Justin Sonntag";
         email = mkDefault "justin.sonntag@gmail.com";
       };
-      ui.diff-formatter = ["${pkgs.difftastic}/bin/difft" "--color=always" "$left" "$right"];
+      ui = {
+        diff-formatter = ["${pkgs.difftastic}/bin/difft" "--color=always" "$left" "$right"];
+        default-command = "status";
+      };
     };
+  };
+
+  home.shellAliases = {
+    je = "${pkgs.jujutsu}/bin/jj edit";
+    jl = "${pkgs.jujutsu}/bin/jj log";
+    jn = "${pkgs.jujutsu}/bin/jj new --no-edit";
+    jne = "${pkgs.jujutsu}/bin/jj new";
+    jbc = "${pkgs.jujutsu}/bin/jj bookmark create";
+
+    # fetch and push
+    jf = "${pkgs.jujutsu}/bin/jj git fetch";
+    jp = "${pkgs.jujutsu}/bin/jj git push -Nr \"fork_point(trunk()..@)-::@ | @::\"";
+
+    # rebase
+    jr = "${pkgs.jujutsu}/bin/jj rebase";
+
+    # diffs
+    jd = "jdn | ${pkgs.fzf}/bin/fzf --preview \"${pkgs.jujutsu}/bin/jj diff {}\" --height=100% --preview-window=up:80%";
+    jdn = "${pkgs.jujutsu}/bin/jj diff --name-only";
+
+    # diffs from main
+    jdm = "jdmn | ${pkgs.fzf}/bin/fzf --preview \"${pkgs.jujutsu}/bin/jj diff -f \\\"fork_point(trunk()..@)-\\\" {}\" --height=100% --preview-window=up:80%";
+    jdmn = "${pkgs.jujutsu}/bin/jj diff -f \"fork_point(trunk()..@)-\" --name-only --no-pager";
   };
 }
