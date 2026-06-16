31a05dd92f8a3e7bf41fe174931c55731dc5efee
Justin Sonntag <justin.sonntag@gmail.com>
2026-06-16 11:57:13 -0700

    Disable some zellij config

--- modules/shell/zellij/zellij.nix
+++ modules/shell/zellij/zellij.nix
@@ -2,10 +2,15 @@
   den.aspects.justin.homeManager = {lib, ...}: {
     programs.zellij = {
       enable = true;
-      enableFishIntegration = true;
+      # enableFishIntegration = true;
+      # attachExistingSession = true;
+      # exitShellOnExit = true;
       themes.rose-pine = ./rose-pine.kdl;
       settings = {
         theme = "rose-pine";
+
+        # pane_frames = false;
+        # default_mode = "locked";
       };
     };
   };
