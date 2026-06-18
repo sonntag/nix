f65af34d9331867859112cd9d30c4daf3438de68
Justin Sonntag <justin.sonntag@gmail.com>
2026-06-18 11:14:12 -0700

    Remove options to change nix distros on darwin

--- modules/nix.nix
+++ modules/nix.nix
@@ -3,25 +3,12 @@
     config,
     inputs,
     lib,
-    pkgs,
     ...
   }:
     with lib; let
       cfg = config.sonntag.nix;
-      isDeterminate = cfg.provider == "determinate";
-      nix-packages = {
-        "determinate" = inputs.determinate.inputs.nix.packages.aarch64-darwin.default;
-        "nix" = pkgs.nix;
-        # TODO: This should be set in an overlay: https://lix.systems/add-to-config/#flake-based-configurations
-        "lix" = pkgs.lixPackageSets.stable.lix;
-      };
     in {
       options.sonntag.nix = {
-        provider = mkOption {
-          type = lib.types.enum ["determinate" "nix" "lix"];
-          default = "determinate";
-          description = "The provider type for the nix system";
-        };
         trusted-users = mkOption {
           type = types.listOf types.str;
           default = ["root" "justin"];
@@ -30,24 +17,11 @@
       };
       config = {
         nix = {
-          enable = !isDeterminate;
-          package = nix-packages.${cfg.provider};
-          settings = mkIf (!isDeterminate) {
-            inherit (cfg) trusted-users;
-            substituters = [
-              "https://cache.lix.systems"
-            ];
-            trusted-public-keys = [
-              "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-            ];
-            experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
-          };
+          enable = false;
+          package = inputs.determinate.inputs.nix.packages.aarch64-darwin.default;
         };
 
-        determinateNix.customSettings = mkIf isDeterminate {
+        determinateNix.customSettings = {
           inherit (cfg) trusted-users;
           # Enables parallel evaluation (remove this setting or set the value to 1 to disable)
           eval-cores = 0;
