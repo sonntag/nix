7e2c433e9e7bbf28bfc6ac77139cc04892fa04f4
Justin Sonntag <justin.sonntag@gmail.com>
2026-06-20 12:15:25 -0700

    Install aloha

--- modules/programs/homebrew/personal-casks.nix
+++ modules/programs/homebrew/personal-casks.nix
@@ -2,6 +2,7 @@
   # These are the casks that should only be on my personal macs
   den.aspects.personal.darwin.config.homebrew.casks = [
     "ableton-live-suite"
+    "aloha-browser"
     "bambu-studio"
     "blackhole-2ch"
     "dropbox"
